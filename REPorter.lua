048d8a90a8211f3685dc03a60392431502e7b90b
Paweł Jastrzębski <pawelj@iosphe.re>
2023-07-12 08:39:39 +0200

    TOC bump

--- REPorter.lua
+++ REPorter.lua
@@ -114,7 +114,7 @@ RE.BlinkPOIValue = 0.3
 RE.BlinkPOIUp = true
 
 RE.FoundNewVersion = false
-RE.AddonVersionCheck = 298
+RE.AddonVersionCheck = 299
 RE.ScreenHeight, RE.ScreenWidth = _G.UIParent:GetCenter()
 
 RE.MapSettings = {
